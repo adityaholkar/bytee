4a91352a2fdad0248dd6d4d5404fc35299164953
aditya <aditya.holkar17@vit.edu>
2021-02-27 12:08:54 +0530

    Test case for empty string

--- testCalculator/src/testCalculator/testCalculator.java
+++ testCalculator/src/testCalculator/testCalculator.java
@@ -1,5 +1,12 @@
 package testCalculator;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 
+import org.junit.Test;
 public class testCalculator {
-
+	@Test
+	public void sumEmptyStringAnswer0(){
+		assertThat(	stringCalculator.Add(""),is(0));
+	}
+	
 }
